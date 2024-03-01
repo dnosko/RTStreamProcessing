2d20f1b212502afa89875aeeaf9e4fcaa9505271
Daša Nosková <dasa.noskova@gmail.com>
2024-03-01 21:05:07 +0100

    mapping rt api

--- api/realtime_locations/rt_locations.py
+++ api/realtime_locations/rt_locations.py
@@ -1,6 +1,7 @@
 import sqlalchemy as db
 from sqlalchemy import select, Table, Column, Integer, MetaData, exc
 import redis
+from redis import Redis
 from fastapi import FastAPI, Path, Query
 import schemas_rt as _schemas
 from typing import Optional, List
@@ -12,7 +13,8 @@ BAD_REQUEST_ERROR = 400
 
 api = FastAPI()
 # TODO config
-redis_client = redis.Redis(host='localhost', port=6379, decode_responses=True)
+redis_client = redis.StrictRedis(host='localhost', port=6379, db=0, decode_responses=True)
+#redis_cache = Redis(host='localhost', port=6379, db=1)
 engine = db.create_engine("postgresql://postgres:password@localhost:25432/data")
 
 metadata = MetaData()
@@ -42,7 +44,6 @@ def get_actual_location(user: List[int] = Query(..., title="User IDs")):
             keys = [device[0] for device in devices]
             # get locations from redis
             values = redis_client.mget(keys)
-            print(values)
 
             # jsonify
             return parse(values, _schemas.Location)
@@ -51,10 +52,27 @@ def get_actual_location(user: List[int] = Query(..., title="User IDs")):
             return {"id": e.code, "description": descr, "http_response_code": INTERNAL_SERVER_ERROR}
 
 
-@api.get("/rt/points/", response_model=List[_schemas.Location])
+# returns all actual points
+@api.get("/rt/points/", response_model=List[_schemas.LocationUser])
 def points_on_map():
-
     all_keys = redis_client.keys('*')
-    all_records = [(redis_client.get(key)) for key in all_keys]
 
-    return parse(all_records, _schemas.Location)
+    query = select([users]).where(users.c.device.in_(all_keys))
+    with engine.connect() as conn:
+        result = conn.execute(query)
+        records = result.fetchall()
+
+    # map the user id to device id
+    mapping = {f"{device_id}": user_id for user_id, device_id in records}
+
+    # get records and map key (device id) to get user id
+    all_records = [(mapping[key], redis_client.get(key)) for key in all_keys]
+
+    locations = []
+    for user, value in all_records:
+        data = json.loads(value)
+        data['device_id'] = data.pop('id')
+        location = _schemas.LocationUser(user_id=user, **data)
+        locations.append(location)
+
+    return locations
--- api/realtime_locations/schemas_rt.py
+++ api/realtime_locations/schemas_rt.py
@@ -3,11 +3,20 @@ from typing import Tuple
 
 from sqlalchemy import DateTime
 
-class Point (BaseModel):
+
+class Point(BaseModel):
     x: float
     y: float
 
+
 class Location(BaseModel):
     id: int
+    point: Point
+    timestamp: int
+
+
+class LocationUser(BaseModel):
+    user_id: int
+    device_id: int
     point: Point
     timestamp: int
