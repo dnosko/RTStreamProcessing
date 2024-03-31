ff20a31372dff09e587bba49402cfdcadbd8a995
Daša Nosková <dasa.noskova@gmail.com>
2024-03-31 11:37:34 +0200

    test data

--- generator/main.py
+++ generator/main.py
@@ -1,17 +1,31 @@
 from Generator import Generator
 
+import gzip
+import json
+
 import argparse
 import asyncio
 async def main(args):
 
     generator = Generator(devices=args.num_dev, limit_x=args.limit_x, limit_y=args.limit_y, uri=args.ws, limit_cnt=args.limit)
-
-    try:
-        await generator.hello()
-        await generator.send_data()
-    finally:
-        await generator.stop()
-        asyncio.get_event_loop().stop()
+    # Generate data to file (for testing)
+    if args.file:
+        if args.limit is None:
+            print("Limit is required with writing to file.")
+            exit(0)
+        else:
+            # use compressed gz file
+            gz_file_name = args.file if args.file.endswith('.gz') else f"{args.file}.gz"
+            with gzip.open(gz_file_name, 'wt') as file:
+                for data in generator.gen_data(args.limit):
+                    file.write(data + "\n")
+    else:
+        try:
+            await generator.hello()
+            await generator.send_data()
+        finally:
+            await generator.stop()
+            asyncio.get_event_loop().stop()
 
 
 if __name__ == "__main__":
@@ -22,6 +36,7 @@ if __name__ == "__main__":
     parser.add_argument('--limit_y', type=str, default='0.0,100.0', help='Latitude interval. If not specified, its 0-100.0', required=False)
     parser.add_argument('--ws', type=str, default='ws://localhost:8088/ws', help='Websocket connection uri. If not specified, "ws://localhost:8088/ws" is used.', required=False)
     parser.add_argument('--limit', type=int, help='Limit number of generated records.', required=False)
+    parser.add_argument('--file', type=str, required=False)
 
     args = parser.parse_args()
     # Convert limit_x and limit_y from string to tuple
